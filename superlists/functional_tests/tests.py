049f92aac342160b8cb392bb4a8b03d3722fc19d
Kenneth <kcheung331@gmail.com>
2019-10-03 13:38:28 -0700

    docker update to use selenium_hub

---
+++ superlists/functional_tests/tests.py
@@ -0,0 +1,90 @@
+# from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+# from django.test import override_settings
+# from selenium import webdriver
+# from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+# from selenium.webdriver.common.keys import Keys
+# from urllib.parse import urlparse
+# import socket
+# import time
+# import os
+# import unittest
+
+# @override_settings(ALLOWED_HOSTS=['*'])
+# class BaseTestCase(StaticLiveServerTestCase):
+# 	"""
+# 	Provides base test class which connects to the Docker
+# 	container running selenium.
+# 	"""
+
+# 	os.environ['DJANGO_LIVE_TEST_SERVER_ADDRESS'] = '0.0.0.0:8000'
+
+# 	@classmethod
+# 	def setUpClass(cls):
+# 		super().setUpClass()
+# 		cls.host = socket.gethostbyname(socket.gethostname())
+# 		cls.browser = webdriver.Remote(
+# 			command_executor='http://selenium:4444/wd/hub',
+#             desired_capabilities=DesiredCapabilities.CHROME,
+# 		)
+# 		cls.live_server_url = 'http://{}:8000'.format(cls.host)
+# 		cls.browser.implicitly_wait(5)
+
+# 	@classmethod
+# 	def tearDownClass(cls):
+# 		cls.browser.quit()
+# 		super().tearDownClass()
+
+
+# class NewVisitorTest(BaseTestCase):
+
+# 	def check_for_row_in_list_table(self, row_text):
+# 		table = self.browser.find_element_by_id('id_list_table')
+# 		rows = table.find_elements_by_tag_name('tr')
+# 		self.assertIn('1: Buy peacock feathers',[row.text for row in rows])
+
+
+# 	def test_can_start_a_list_and_retrieve_it_later(self):
+# 		"""
+# 		Edith has heard about a cool new online to-do app. She goes
+# 		to check it out
+# 		"""
+# 		# self.browser.get("{}{}".format(self.live_server_url,'/admin'))
+# 		self.browser.get(self.live_server_url)
+
+# 		# she notices the page title and header mention to-do lists
+# 		self.assertIn('To-Do', self.browser.title)
+# 		header_text = self.browser.find_element_by_tag_name('h1').text
+# 		self.assertIn('To-Do', header_text)
+
+# 		# she is invited to enter a to-do item straight away
+# 		inputbox = self.browser.find_element_by_id('id_new_item')
+# 		self.assertEqual(
+# 			inputbox.get_attribute('placeholder'),
+# 			'Enter a to-do item'
+# 		)
+
+# 		# she types "Buy peacock feathers" into a text box (Edith's hobby)
+# 		inputbox.send_keys('Buy peacock feathers')
+
+# 		# when she hits enter, the page updates, and now the page lists
+# 		# "1: Buy peacock feathers" as an item in the to-do list table
+# 		inputbox.send_keys(Keys.ENTER)
+
+# 		table = self.browser.find_element_by_id('id_list_table')
+# 		rows = table.find_elements_by_tag_name('tr') #notice elements plural
+# 		self.assertIn('1: Buy peacock feathers',[row.text for row in rows])
+
+# 		# There is still a text box inviting her to add another item. She
+# 		# enters "Use peacock feathers to make a fly"
+# 		inputbox = self.browser.find_element_by_id('id_new_item')
+# 		inputbox.send_keys('Use peacock feathers to make a fly')
+# 		inputbox.send_keys(Keys.ENTER)
+
+# 		# the page updates again, and now she has both items on the list
+# 		table = self.browser.find_element_by_id('id_list_table')
+# 		rows = table.find_elements_by_tag_name('tr') #notice elements plural
+
+# 		self.check_for_row_in_list_table('1: Buy peacock feathers')
+# 		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+
+# 		self.fail('Finish the test!')
